298b9332016c5980d5e69d7af6285d9241c4c5b2
haru <hrtkszk@gmail.com>
2021-10-17 14:30:25 +0900

    add note function

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -29,7 +29,11 @@ const awsmobile = {
     },
     "aws_cognito_verification_mechanisms": [
         "EMAIL"
-    ]
+    ],
+    "aws_appsync_graphqlEndpoint": "https://34elybexxvfinkx3rxv7qttyga.appsync-api.ap-northeast-1.amazonaws.com/graphql",
+    "aws_appsync_region": "ap-northeast-1",
+    "aws_appsync_authenticationType": "API_KEY",
+    "aws_appsync_apiKey": "da2-imj6vlmtfzg5hn6rgdbman5h4i"
 };
 
 
